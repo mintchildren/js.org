9a25dba7eb156a7ef1edad58f89f45db2ec13fe3
mintchildren <50468438+mintchildren@users.noreply.github.com>
2019-05-10 01:06:42 +0000

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1764,7 +1764,8 @@ var cnames_active = {
   "zombie": "assaf.github.io/zombie", // noCF? (don´t add this in a new PR)
   "zonayed": "zonayedpca.github.io/js.zonayed.me",
   "zty": "zetaoyang.github.io",
-  "zyy": "zyyou.github.io/notes"
+  "zyy": "zyyou.github.io/notes",
+  "mt": "mtcd.github.io",
   /*
   * please don't add your subdomain records down here!
   * insert them in alphabetical order to help reduce merge conflicts.
